4983550a948a1dd710742b80c7e2590824eb4063
sunilrathour28 <sunilrathour911@gmail.com>
2018-03-30 15:29:43 +0530

    change database name

--- database/dbconf.php
+++ database/dbconf.php
@@ -1,5 +1,5 @@
 <?php
 define('HOST_NAME','localhost');
 define('HOST_USER','root');
-define('HOST_PASSWORD','admin');
-define('DBNAME','gmail');
+define('HOST_PASSWORD','ankit@123');
+define('DBNAME','sunil-project');
